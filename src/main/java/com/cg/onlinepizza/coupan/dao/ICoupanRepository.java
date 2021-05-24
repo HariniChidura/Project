bfac6896e2b5a506c848399f3edadbbdec1667ef
HariniChidura <82203098+HariniChidura@users.noreply.github.com>
2021-05-24 11:52:31 +0530

    Update ICoupanRepository.java

--- src/main/java/com/cg/onlinepizza/coupan/dao/ICoupanRepository.java
+++ src/main/java/com/cg/onlinepizza/coupan/dao/ICoupanRepository.java
@@ -1,17 +1,12 @@
 package com.cg.onlinepizza.coupan.dao;
 
-import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.onlinepizza.coupan.dto.Coupan;
-import com.cg.onlinepizza.exceptions.CoupanIdNotFoundException;
-import com.cg.onlinepizza.exceptions.InvalidCoupanOperationException;
 
-public interface ICoupanRepository {
-	Coupan addCoupans(Coupan coupan);
 
-	Coupan editCoupans(Coupan coupan)throws InvalidCoupanOperationException ;
+public interface ICoupanRepository extends CrudRepository<Coupan,Integer> {
 
-	Coupan deleteCoupans(int coupanId)throws CoupanIdNotFoundException;
-
-	List<Coupan> viewCoupans();
 }
